aa54cf5695df135d86cbb39f064fa6b0b9575184
aswath107 <aswathas118@gmail.com>
2025-02-04 21:05:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x==0){
+            return true;
+        }
+        if(x<0||x%10==0){
+            return false;
+        }
+        int r=0;
+        while(x>r){
+            int sum=x%10;
+            x/=10;
+            r=(r*10)+sum;
+        }
+        if(x==r){
+            return true;
+        }
+        else{
+        return false;}
+    }
+}
