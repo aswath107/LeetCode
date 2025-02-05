13492fa436488ecd6b0d750b66daed66ad06ca0d
aswath107 <aswathas118@gmail.com>
2025-02-05 21:37:43 +0530

    Time: 2 ms (97.65%), Space: 41.8 MB (48.78%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character>Stack=new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c=='(')
+            Stack.push(')');
+            else if(c=='{')
+            Stack.push('}');
+            else if(c=='[')
+            Stack.push(']');
+            else if(Stack.isEmpty() || Stack.pop() !=c)
+            return false;
+        }
+        return Stack.isEmpty();
+    }
+}
